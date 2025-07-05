5d547521a7567c8874750b576ee726289e37d471
kayqueR69 <kayquericharlyson@gmail.com>
2025-07-05 09:07:40 -0300

    add: criação do controller parar profissionais e arquivo de rotas

--- innerly/urls.py
+++ innerly/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('innerlyapp.routes.usuarioUrls')),
-    path('registros/', include('innerlyapp.routes.registroUrls'))
+    path('registros/', include('innerlyapp.routes.registroUrls')),
+    path('profissionais/', include('innerlyapp.routes.profissionalUrls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ innerlyapp/routes/profissionaisUrls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from innerlyapp.controllers.profissionalController import *
+
+urlpatterns = [
+    
+]
